a9df6c53213e1abd734b242e7904d25c1e4a6e3e
Ken Patrick Garcia <kenpatrickgarcia123@gmail.com>
2026-01-12 02:22:28 +0800

    chore: updated experience

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,26 @@ import Hero from "./sections/Hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 import NavBar from "./components/NavBar.jsx";
 import CertificationsSection from "./sections/CertificationsSection.jsx";
-import FeatureCards from "./sections/FeatureCards.jsx";
 import ExperienceSection from "./sections/ExperienceSection.jsx";
 import TechStack from "./sections/TechStack.jsx";
 import Testimonials from "./sections/Testimonials.jsx";
 import Contact from "./sections/Contact.jsx";
 import Footer from "./sections/Footer.jsx";
+import { ThemeProvider } from "./contexts/ThemeContext.jsx";
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider>
       <NavBar />
       <Hero />
       <ShowcaseSection />
       <CertificationsSection />
-      <FeatureCards />
       <ExperienceSection />
       <TechStack />
       <Testimonials />
       <Contact />
       <Footer />
-    </>
+    </ThemeProvider>
   );
 };
 export default App;
